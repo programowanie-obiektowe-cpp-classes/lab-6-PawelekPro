bb508380f65d908f4a345848f69d4fb8320f5df5
PawelekPro <pawel.gilewicz1@gmail.com>
2022-12-19 15:30:44 +0100

    fixed?

--- foo.hpp
+++ foo.hpp
@@ -9,7 +9,8 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
-    std::transform(people.begin(), people.end(), ret_v.rbegin(), [](Human& human){human.birthday(); char tmp = human.isMonster() ? 'n': 'y'; return tmp;});
+
+    std::transform(people.begin(), people.end(), ret_v.rbegin(), [](Human& human){human.birthday(); char temp = human.isMonster()? 'n':  'y'; return temp;});
 
     return ret_v;
 }
